1ea85b20b656eede264f1daa6249ed3803651dbd
DhivagarT <146562198+DhivagarT@users.noreply.github.com>
2025-10-11 12:28:32 +0530

    Add files via upload

---
+++ DAY2/full_adder/full_adder_tb.v
@@ -0,0 +1,23 @@
+module full_adder_tb();
+reg a,b,cin;
+wire sum,carry;
+
+full_adder dut(.a(a),.b(b),.cin(cin),.sum(sum),.carry(carry));
+
+initial begin
+$monitor("Time=%0t a=%b b=%b cin=%b sum=%b carry=%b",$time,a,b,cin,sum,carry);
+$dumpfile("full_adder_wave.vcd");
+$dumpvars(8,full_adder_tb);
+
+a=0;b=0;cin=0;
+#10 a=0;b=0;cin=1;
+#10 a=0;b=1;cin=0;
+#10 a=0;b=1;cin=1;
+#10 a=1;b=0;cin=0;
+#10 a=1;b=0;cin=1;
+#10 a=1;b=1;cin=0;
+#10 a=1;b=1;cin=1;
+#10;
+$finish;
+end
+endmodule
