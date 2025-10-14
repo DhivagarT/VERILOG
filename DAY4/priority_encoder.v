8957f71999ad442576ed1f9024145c8a592c85dd
DhivagarT <dhivagardhanavel07@gmail.com>
2025-10-14 06:05:07 +0000

    initial commit

--- DAY4/priority_encoder.v
+++ DAY4/priority_encoder.v
@@ -1,8 +1,7 @@
 module priority_encoder(input d3,d2,d1,d0,output [1:0]a,output v);
 
 assign v= d3|d2|d1|d0;
-assign a[1] = d3 | (d2 & ~d3);
-assign a[0] = d3 | (d1 & ~d3 & ~d2);
 
+assign a= (d3)? 2'b11: (d2)? 2'b10: (d1)? 2'b01: (d0)? 2'b00: 2'bxx;
 endmodule
    
